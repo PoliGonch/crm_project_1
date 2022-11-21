df4e4719d5518ce59641327a9d9c48f2e0151da5
polina <borisovapolina5@gmail.com>
2022-11-22 01:29:42 +0200

    :rocket: Add changes to settings

--- crm_project/settings.py
+++ crm_project/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crm',
     'rest_framework',
-    'rest_framework_simplejwt',
+    # 'rest_framework_simplejwt',
     'corsheaders',
 
 ]
