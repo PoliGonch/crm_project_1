2ab8b2e474cfe62a7e5e0992e54a8ea95008f3c1
polina <borisovapolina5@gmail.com>
2022-11-22 01:58:14 +0200

    :rocket: Add changes to settings

--- crm_project/wsgi.py
+++ crm_project/wsgi.py
@@ -10,9 +10,13 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crm_project.settings')
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv()
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crm_project.settings')
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crm_project.settings.local")
-os.environ["DJANGO_SETTINGS_MODULE"] = "crm_project.settings"
+# os.environ["DJANGO_SETTINGS_MODULE"] = "crm_project.settings"
 
 application = get_wsgi_application()
