032e7c2572bd5c86e760efa367ab330353fa4060
polina <borisovapolina5@gmail.com>
2022-11-22 01:20:52 +0200

    :rocket: Add changes to wsgi and settings

--- crm_project/wsgi.py
+++ crm_project/wsgi.py
@@ -12,6 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crm_project.settings')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crm_project.settings.local")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crm_project.settings.local")
+os.environ["DJANGO_SETTINGS_MODULE"] = "crm_project.settings"
 
 application = get_wsgi_application()
