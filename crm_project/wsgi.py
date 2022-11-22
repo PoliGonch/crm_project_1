8c64cf59ccf224fc379717f7af07a7bb6518d400
polina <borisovapolina5@gmail.com>
2022-11-22 02:17:58 +0200

    :rocket: Add changes to settings

--- crm_project/wsgi.py
+++ crm_project/wsgi.py
@@ -8,11 +8,13 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
 from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+path_to_parent_file = Path(__file__).resolve().parent
+dotenv_path = os.path.join(os.path.dirname(path_to_parent_file), '.env')
 load_dotenv(dotenv_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crm_project.settings')
